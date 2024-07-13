d1737b369b766bf127b4ccb4534894632e28f437
Αναστάσης Καμπέρος <85846505+Tatsoskab@users.noreply.github.com>
2024-07-13 21:53:06 +0300

    Update main.py
    works better

--- main.py
+++ main.py
@@ -3,26 +3,16 @@ import os
 import time
 import yt_dlp
 
-
-#Youtube to video
+# YouTube to video
 def download_video(url):
     ydl_opts = {
         'outtmpl': 'video.%(ext)s',
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-        
+        info_dict = ydl.extract_info(url, download=True)
+        return info_dict['ext']
 
-if __name__ == "__main__":
-    video_url = input("Enter YouTube video URL: ")
-    download_video(video_url)
-
-time.sleep(3)
-
-
-
-
-#Video to mp3
+# Video to mp3
 def convert_video_to_mp3(input_file, output_file):
     ffmpeg_cmd = [
         "ffmpeg",
@@ -36,28 +26,41 @@ def convert_video_to_mp3(input_file, output_file):
     ]
 
     try:
-        subprocess.run(ffmpeg_cmd, check = True)
+        subprocess.run(ffmpeg_cmd, check=True)
         print("SUCCESS")
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError:
         print("FAIL")
 
-convert_video_to_mp3("video.webm", "audio.mp3")
+if __name__ == "__main__":
+    video_url = input("Enter YouTube video URL: ")
+    video_ext = download_video(video_url)
 
-#move audio to folder
-destination = "music/audio.mp3"
-source = "audio.mp3"
-try:
-    if os.path.exists(destination):
-        print("There is already a file there")
+    time.sleep(3)
+
+    video_file = f"video.{video_ext}"
+    convert_video_to_mp3(video_file, "audio.mp3")
+
+    # Move audio to folder
+    if not os.path.exists('music'):
+        os.makedirs('music')
+
+    destination = "music/audio.mp3"
+    source = "audio.mp3"
+    try:
+        if os.path.exists(destination):
+            print("There is already a file there")
+        else:
+            os.replace(source, destination)
+            print(source + " was moved successfully")
+            # Rename audio file
+            music = input("Name the audio file: ") + ".mp3"
+            os.rename(destination, os.path.join("music", music))
+            print(f"File renamed to {music}")
+    except FileNotFoundError:
+        print("Source not found")
+
+    # Remove video
+    if os.path.exists(video_file):
+        os.remove(video_file)
     else:
-        os.replace(source,destination)
-        print(source +" was moved successfully")
-        #rename audio file
-        music = input("Name the audio file: ") + ".mp3"
-        os.rename(destination, os.path.join("music", music))
-        print(f"File renamed to {music}")
-except FileNotFoundError:
-    print("source not found")
-
-os.remove("video.webm")
-
+        print("Downloaded video file not found")
